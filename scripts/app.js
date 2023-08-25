fa7c96b1e2e79e5bcb700d11ebe0bc1f14babec5
25hokins25 <25hokins25@gmail.com>
2023-08-25 12:37:39 -0700

    Add scroll function

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,10 @@ window.onload = () => {
 	}
 }
 
+window.addEventListener('scroll', e => {
+	document.body.style.cssText = `--scrollTop: `
+})
+
 
 document.addEventListener('mousemove', el => {
 	Object.assign(document.documentElement, {
